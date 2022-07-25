0e7d82efb11487f77082686ce247f4cac0a4f17b
Your Name <you@example.com>
2022-07-25 19:55:01 +0000

    redact doc ...

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -29,70 +29,41 @@ const Home = () => {
         <div className="folders">
           
            
-        {/* <Card title={"Folder Structure"} style={{margin: 20}}> */}
-        <Card style={{margin: 20}}>
+            
+          {/* <Card title={"Folder Structure"} style={{ margin: 20 }}>
+            <FolderStructure />
+          </Card> */}
 
-        <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-      probare, quae sunt a te dicta? Refert tamen, quo modo.
-    </p>
-    <Divider  />
+        <Card >
          
-        <table >
-<tbody>
- 
-<tr><td>📦uber-eats-clone</td></tr>
-<tr><td> ┣ 📂assets</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Any file that the app need at runtime, contain all static image files, icons, animations, fonts </i></td></tr>
+        <h1>Setting up the development environment</h1>
 
-<tr><td> ┣ 📂components</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain all components shared between screens, all applications, reusable and UI components </i></td></tr>
-<tr><td> ┣ 📂contexts</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Context is designed to share data that can be considered global to a component tree </i></td></tr>
-<tr><td> ┣ 📂navigation</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain all stack navigators, to manage the navigation history and presentation of appropriate screen  </i></td></tr>
-<tr><td> ┣ 📂node_modules</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Directory created by npm and a way of tracking each installed packages. A cache for the external modules  </i></td></tr>
-<tr><td> ┣ 📂redux</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain reducers, actions , state container, a store to store the state of the variables  </i></td></tr>
-<tr><td> ┣ 📂screens</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>contain all screens of app, components can be handled by some navigator  </i></td></tr>
-<tr><td> ┣ 📜.gitignore</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Specifies untracked files that git should ignore. Files already tracked by git are not affected </i></td></tr>
-<tr><td> ┣ 📜App.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Main component, which acts as a container for all other components . </i></td></tr>
-<tr><td> ┣ 📜README.md</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain information that is commonly required to understand what the project is about</i></td></tr>
-<tr><td> ┣ 📜babel.config.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Run plugins on a subset of files, convert syntax into code that can be run in javascript environment</i></td></tr>
-<tr><td> ┣ 📜data.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain some data use by many components in all app </i></td></tr>
-<tr><td> ┣ 📜firebase.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain firebase configuration, functions, interactions with database, all firebase services</i></td></tr>
-<tr><td> ┣ 📜global.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain all variables used in app globally, constant, currency, global style, api keys, language ... </i></td></tr>
-<tr><td> ┣ 📜package.json</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Manifest file for app, metadata associated with project + all dependencies with version + scripts</i></td></tr>
-<tr><td> ┣ 📜utils.js</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Contain all util functions , geolocation, distance between points, generate uid ...   </i></td></tr>
-<tr><td> ┗ 📜yarn.lock</td>
-<td><i style={{marginLeft: 20,color:"grey"}}>Manage and list the versions of dependencies that are used at the time of the installation process</i></td></tr>
-</tbody>
-</table>
+        <hr />
 
-          {/* <table style={{marginLeft: 40}}>
-          <tbody>
-          <tr><td>📦.expo
-            <i style={{marginLeft: 20,color:"grey"}}>All components share between all screens</i></td>
-          </tr>
-          <tr><td>┣📜README.md
-            <i style={{marginLeft: 20,color:"grey"}}>All components share between all screens</i></td>
-          </tr>
-          <tr><td> ┣ 📂home <i style={{marginLeft: 20,color:"grey"}}>Components in Home screen</i></td></tr>
-          <tr><td> ┣ 📂restaurantDetail <i style={{marginLeft: 20,color:"grey"}}>Components in Restaurant details screen</i></td></tr>
-           
-          </tbody>
-          </table> */}
+<p>This page will help you install and build your first React Native app.</p>
+
+<p><strong>If you are new to mobile development</strong>, the easiest way to get started is with Expo CLI. Expo is a set of tools built around React Native and, while it has many&nbsp;<a href="https://expo.io/features" rel="noopener noreferrer" target="_blank">features</a>, the most relevant feature for us right now is that it can get you writing a React Native app within minutes. You will only need a recent version of Node.js and a phone or emulator. If you&#39;d like to try out React Native directly in your web browser before installing any tools, you can try out&nbsp;<a href="https://snack.expo.dev/" rel="noopener noreferrer" target="_blank">Snack</a>.</p>
+
+
+<p>&nbsp;</p>
+
+<h4>STEP 1 - Install Node.js</h4>
+
+<p>If you have the Node 10 LTS or greater installed on your machine. kindly move to the next step!, If you dont, visit&nbsp;<a href="https://nodejs.org/en/">nodejs website</a>&nbsp;to install the recommended version for your OS.</p>
+
+<p>&nbsp;</p>
+
+<h4>STEP 2 - Install the Expo CLI</h4>
+
+<p>We&#39;ll install Expo CLI with npm, npm comes along with Node, you can confirm this by running the following command.</p>
+
+<p>&nbsp;</p>
+
+
+
+
+
+
         </Card>
 
  
@@ -108,5 +79,51 @@ const Home = () => {
 };
 
 export default Home;
+
+const FolderStructure = () => {
+  return (
+    <table >
+    <tbody>
+     
+    <tr><td>📦uber-eats-clone</td></tr>
+    <tr><td> ┣ 📂assets</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Any file that the app need at runtime, contain all static image files, icons, animations, fonts </i></td></tr>
+    
+    <tr><td> ┣ 📂components</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain all components shared between screens, all applications, reusable and UI components </i></td></tr>
+    <tr><td> ┣ 📂contexts</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Context is designed to share data that can be considered global to a component tree </i></td></tr>
+    <tr><td> ┣ 📂navigation</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain all stack navigators, to manage the navigation history and presentation of appropriate screen  </i></td></tr>
+    <tr><td> ┣ 📂node_modules</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Directory created by npm and a way of tracking each installed packages. A cache for the external modules  </i></td></tr>
+    <tr><td> ┣ 📂redux</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain reducers, actions , state container, a store to store the state of the variables  </i></td></tr>
+    <tr><td> ┣ 📂screens</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>contain all screens of app, components can be handled by some navigator  </i></td></tr>
+    <tr><td> ┣ 📜.gitignore</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Specifies untracked files that git should ignore. Files already tracked by git are not affected </i></td></tr>
+    <tr><td> ┣ 📜App.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Main component, which acts as a container for all other components . </i></td></tr>
+    <tr><td> ┣ 📜README.md</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain information that is commonly required to understand what the project is about</i></td></tr>
+    <tr><td> ┣ 📜babel.config.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Run plugins on a subset of files, convert syntax into code that can be run in javascript environment</i></td></tr>
+    <tr><td> ┣ 📜data.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain some data use by many components in all app </i></td></tr>
+    <tr><td> ┣ 📜firebase.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain firebase configuration, functions, interactions with database, all firebase services</i></td></tr>
+    <tr><td> ┣ 📜global.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain all variables used in app globally, constant, currency, global style, api keys, language ... </i></td></tr>
+    <tr><td> ┣ 📜package.json</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Manifest file for app, metadata associated with project + all dependencies with version + scripts</i></td></tr>
+    <tr><td> ┣ 📜utils.js</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Contain all util functions , geolocation, distance between points, generate uid ...   </i></td></tr>
+    <tr><td> ┗ 📜yarn.lock</td>
+    <td><i style={{marginLeft: 20,color:"grey"}}>Manage and list the versions of dependencies that are used at the time of the installation process</i></td></tr>
+    </tbody>
+    </table>
+  )
+}
 
  
