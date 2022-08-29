3a3c5ae3a7fba842d7bbf57578d84c570f41344c
nass <cabliveer@gmail.com>
2022-08-30 00:31:12 +0200

    s

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,6 @@
  import Sidebar from "../../components/sidebar/Sidebar";
  import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
-import Navbar1 from "../../components/navbar1/Navbar1";
  import Widget from "../../components/widget/Widget";
  import Featured from "../../components/featured/Featured";
  import Chart from "../../components/chart/Chart";
