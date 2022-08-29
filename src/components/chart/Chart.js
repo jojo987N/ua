4fdd0e8f029d9d7cdf1d15e55973d9c70f809049
nass <cabliveer@gmail.com>
2022-08-29 16:34:23 +0200

    save

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,6 +5,9 @@ import {
   XAxis,
   CartesianGrid,
   Tooltip,
+
+  PieChart, Pie, Sector, Cell,
+  
   ResponsiveContainer,
 } from "recharts";
 
@@ -23,8 +26,8 @@ const Chart = ({ aspect, title }) => {
       <div className="title">{title}</div>
       <ResponsiveContainer width="100%" aspect={aspect}>
       <PieChart width={730} height={250}>
-  <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-  <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+  <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+  <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
 </PieChart>
       </ResponsiveContainer>
     </div>
