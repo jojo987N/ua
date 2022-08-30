8a44d115defcb9af05b0af18125f063bd7d32591
nass <cabliveer@gmail.com>
2022-08-30 19:16:07 +0200

    gf

--- src/components/pieChart/PieChart.js
+++ src/components/pieChart/PieChart.js
@@ -23,7 +23,7 @@ const data01 = [
 const PieChartt = () => {
     return (
       <div className="pieChart">
-          <ResponsiveContainer width="100%" height="100%">
+          <ResponsiveContainer  width={'99%'} height={400}>
         <PieChart width={400} height={400}>
           <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
           <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
