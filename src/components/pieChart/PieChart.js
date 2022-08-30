a1c23ad54adf0f56458e2da8976fee941305a49d
nass <cabliveer@gmail.com>
2022-08-30 19:13:35 +0200

    fg

--- src/components/pieChart/PieChart.js
+++ src/components/pieChart/PieChart.js
@@ -23,12 +23,12 @@ const data01 = [
 const PieChartt = () => {
     return (
       <div className="pieChart">
-          {/* <ResponsiveContainer width="100%" height="100%"> */}
+          <ResponsiveContainer width="100%" height="100%">
         <PieChart width={400} height={400}>
           <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
           <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
         </PieChart>
-      {/* </ResponsiveContainer> */}
+      </ResponsiveContainer>
       </div>
     )
 }
