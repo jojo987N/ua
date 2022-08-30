c4b70546a69b89ef270524ca1bd6c25874ec527a
nass <cabliveer@gmail.com>
2022-08-30 20:10:19 +0200

    dg

--- src/components/pieChart/PieChart.js
+++ src/components/pieChart/PieChart.js
@@ -23,14 +23,30 @@ const data01 = [
 const PieChartt = () => {
     return (
       <div className="pieChart">
-          {/* <ResponsiveContainer  width={'99%'} height={400}> */}
-        <PieChart width={300} height={300}>
-          <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-          <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-        </PieChart>
-      {/* </ResponsiveContainer> */}
+        <ResponsiveContainer width={"99%"} height={300}>
+          <PieChart width={300} height={300}>
+            <Pie
+              data={data01}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              outerRadius={60}
+              fill="#8884d8"
+            />
+            <Pie
+              data={data02}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              innerRadius={70}
+              outerRadius={90}
+              fill="#82ca9d"
+              label
+            />
+          </PieChart>
+        </ResponsiveContainer>
       </div>
-    )
+    );
 }
 
 export default PieChartt;
