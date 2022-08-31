0d56f82a40ceed368c614dd4a41a7b19b309bba8
nass <cabliveer@gmail.com>
2022-08-31 20:07:00 +0200

    dg

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -294,16 +294,7 @@ const Sidebar = ({ type }) => {
           </li> */}
         </ul>
       </div>
-      <div className="bottom">
-        <div
-          className="colorOption"
-
-        ></div>
-        <div
-          className="colorOption"
-
-        ></div>
-      </div>
+      
     </div>
   );
 };
