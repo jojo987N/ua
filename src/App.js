0995e0558d06b717392bfb05a7932e50fac3e075
nass <cabliveer@gmail.com>
2022-09-02 18:52:59 +0200

    sf

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import Home from "./pages/home/Home";
 
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { carInputs, categoryInputs, productInputs, restaurantInputs, userInputs } from "./formSource";
-import Restaurant from "./pages/restaurant/Restaurant";
-import { AuthProvider } from "./context/Auth";
-import PrivateRoute from "./components/privateRoute/PrivateRoute";
-import { RestaurantProvider } from "./context/RestaurantContext";
-import { LoadingProvider } from "./context/LoadingContext";
-import { AuthContext } from "./context/Auth";
-import Driver from "./pages/driver/Driver";
 
 
 
