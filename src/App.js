de1517e75e23202007999e7109513f60e4752b22
nass <cabliveer@gmail.com>
2022-09-02 18:49:23 +0200

    dg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
 import List from "./pages/list/List";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
-import { decryptData, encryptData } from "./utils";
+
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { carInputs, categoryInputs, productInputs, restaurantInputs, userInputs } from "./formSource";
 import { getOrdersFromFirebase } from './firebase'
@@ -16,22 +13,55 @@ import { RestaurantProvider } from "./context/RestaurantContext";
 import { LoadingProvider } from "./context/LoadingContext";
 import { AuthContext } from "./context/Auth";
 import Driver from "./pages/driver/Driver";
+
+
+
 function App() {
+
+  
+
+  
   return (
+     
+
     <div className="app">
+      
         <BrowserRouter>
+
           <Routes>
             <Route path="/">
+
               <Route index element={
+
+                 
                   <Home />
+                 
               } />
+
+              
+                
+               
+                
+                
+                
+               
+                
+
+              
               </Route>
               <Route path="documentation">
                   <Route index element={<List key="documentation" type="documentation" />} />
               </Route>
+           
           </Routes>
+
         </BrowserRouter>
+       
     </div>
+
+   
+
   );
 }
+
 export default App;
