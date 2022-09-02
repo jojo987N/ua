e58d9ad83adbdd6804f55ea664a1c91f159cb763
nass <cabliveer@gmail.com>
2022-09-02 18:46:36 +0200

    xv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { decryptData, encryptData } from "./utils";
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { carInputs, categoryInputs, productInputs, restaurantInputs, userInputs } from "./formSource";
+import { getOrdersFromFirebase } from './firebase'
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
+import { OrdersContext } from "./context/OrdersContext"
+import Restaurant from "./pages/restaurant/Restaurant";
+import { AuthProvider } from "./context/Auth";
+import PrivateRoute from "./components/privateRoute/PrivateRoute";
+import { RestaurantProvider } from "./context/RestaurantContext";
+import { LoadingProvider } from "./context/LoadingContext";
+import { AuthContext } from "./context/Auth";
+import Driver from "./pages/driver/Driver";
 function App() {
   return (
     <div className="app">
