ecb5166357530e90477bf6d63f2d8d021a76e3f3
nass <cabliveer@gmail.com>
2022-09-05 05:35:12 +0200

    dgfh

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     
     <div className="home">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="homeContainer">
         <Navbar />
         {/* <div className="widgets">
