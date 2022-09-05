46fbf73a6ddb9a279149474b72b0ae92639a48ae
nass <cabliveer@gmail.com>
2022-09-05 02:56:26 +0200

    dg

--- src/config.js
+++ src/config.js
@@ -2,17 +2,25 @@ import {initializeApp} from 'firebase/app'
 
  const firebaseConfig = {
 
-    apiKey: {/* Your firebase config here */},
+    // apiKey: {/* Your firebase config here */},
   
-    authDomain: {/* Your firebase config here */},
+    // authDomain: {/* Your firebase config here */},
   
-    projectId: {/* Your firebase config here */},
+    // projectId: {/* Your firebase config here */},
   
-    storageBucket: {/* Your firebase config here */},
+    // storageBucket: {/* Your firebase config here */},
   
-    messagingSenderId: {/* Your firebase config here */},
+    // messagingSenderId: {/* Your firebase config here */},
   
-    appId: {/* Your firebase config here */}
+    // appId: {/* Your firebase config here */}
+
+    apiKey: "AIzaSyDrKqjM-fKGWBqj0-wpOOrIbeVlViEW-3c",
+    authDomain: "good-food-c84d4.firebaseapp.com",
+    projectId: "good-food-c84d4",
+    storageBucket: "good-food-c84d4.appspot.com",
+    messagingSenderId: "716731554402",
+    appId: "1:716731554402:web:bc8a1748f6cdd6885e8f3b",
+    measurementId: "G-VLK10R4D2P"
   
   };
 
