e1dda6023ac8b25e00e03499c7affc07b2e41890
nass <cabliveer@gmail.com>
2022-09-06 20:53:38 +0200

    df

--- src/pages/new/FormInput.js
+++ src/pages/new/FormInput.js
@@ -62,7 +62,7 @@ class FormInput extends React.Component {
     handleInputChange =  (event) => {
       const target = event.target;
       const name = target.name;
-      console.log(name)
+      console.log(this.state)
       this.setState({
         [name]:  "dgfdhhfj"
       });
