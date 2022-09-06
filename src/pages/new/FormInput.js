ead98cae71093ddbca30b2bd2cbff02a09f3f9fe
nass <cabliveer@gmail.com>
2022-09-06 17:28:21 +0200

    df

--- src/pages/new/FormInput.js
+++ src/pages/new/FormInput.js
@@ -8,27 +8,29 @@ class FormInput extends React.Component {
     render() {
     //   return <div ref={this.myRef} />;
     
-      return this.props.inputs.map((input) => (
-                <div className="formInput" key={input.id}>
-                  <label>{input.label}</label>
-                  <input type={input.type} 
-                  placeholder={input.placeholder}
-                  ref={"input"+input.id}
-                  // onChange={e => setFormData(val => ({
-                  //   ...val,
-                  //   [input.alias] : e.target.value
-                  // }))}
-                //   value={
-                //    // (userId && user[input.alias]) || 
-                //     // (itemId && itemId !== "profile" && (data[input.alias]))} 
-                //   //  itemId !== "profile"?data[input.alias]:input.placeholder} 
+     
 
-                //   // itemId !== "profile"?data?data[input.alias]:"":formData[input.alias]
-                //   formData[input.alias]
-                // } 
-                  />
-                </div>
-              ))
+      // return this.props.inputs.map((input) => (
+      //           <div className="formInput" key={input.id}>
+      //             <label>{input.label}</label>
+      //             <input type={input.type} 
+      //             placeholder={input.placeholder}
+      //             ref={"input"+input.id}
+      //             // onChange={e => setFormData(val => ({
+      //             //   ...val,
+      //             //   [input.alias] : e.target.value
+      //             // }))}
+      //           //   value={
+      //           //    // (userId && user[input.alias]) || 
+      //           //     // (itemId && itemId !== "profile" && (data[input.alias]))} 
+      //           //   //  itemId !== "profile"?data[input.alias]:input.placeholder} 
+
+      //           //   // itemId !== "profile"?data?data[input.alias]:"":formData[input.alias]
+      //           //   formData[input.alias]
+      //           // } 
+      //             />
+      //           </div>
+      //         ))
     
 
     }
