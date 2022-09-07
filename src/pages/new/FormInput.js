37fcc404fe67b639251ff0bb061ba2055c7ad23e
nass <cabliveer@gmail.com>
2022-09-07 20:11:12 +0200

    dg

--- src/pages/new/FormInput.js
+++ src/pages/new/FormInput.js
@@ -4,6 +4,7 @@ import Select from 'react-select'
 import { decryptData } from "../../utils";
 import TimePicker from 'react-time-picker';
 import { addRestaurant, updateUser } from "../../firebase";
+import { restaurantModel } from "../../model";
 
 
 
@@ -293,37 +294,5 @@ class FormInput extends React.Component {
   export default FormInput;
 
 
-  const restaurantModel = (latitude, longitude, phone, image_url, address, city, 
-    country, name) => ({
-      "coordinates":  {
-        "latitude": latitude,
-        "longitude": longitude,
-        
-      },
-      "image_url": image_url,
-      "is_closed": false,
-      "location":  {
-        "address1": address,
-        "address2": "",
-        "address3": null,
-        "city": city,
-        "country": country,
-        "display_address":  [
-          address,
-          "",
-          "",
-        ],
-        "state": "",
-        "zip_code": "",
-      },
-      "name": name,
-      "phone": phone,
-      "price": "$$",
-      "transactions":  [
-        "pickup",
-        "delivery",
-      ],
-      deliveryTime:15,
-      collectTime:5,
-    })
+  
   
