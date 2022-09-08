2ea0e4071d48c4f22f344db4d39b17b8f8fb0de1
nass <cabliveer@gmail.com>
2022-09-08 16:40:49 +0200

    fdf

--- src/pages/new/FormInput.js
+++ src/pages/new/FormInput.js
@@ -74,8 +74,9 @@ class FormInput extends React.Component {
     if (this.props.data)
       updateUser(this.state.inputs, this.props.itemId)
     else
-      addRestaurant(restaurantModel(this.state.inputs.latitude, this.state.inputs.longitude, this.state.inputs.phone, URL.createObjectURL(this.state.inputs.file), this.state.inputs.address, this.state.inputs.city, this.state.inputs.country, this.state.inputs.name))
-    // addRestaurant(this.state.inputs)
+      // addRestaurant(restaurantModel(this.state.inputs.latitude, this.state.inputs.longitude, this.state.inputs.phone, URL.createObjectURL(this.state.inputs.file), this.state.inputs.address, this.state.inputs.city, this.state.inputs.country, this.state.inputs.name))
+      addRestaurant(restaurantModel(this.state.inputs.latitude, this.state.inputs.longitude, this.state.inputs.phone, this.state.inputs.file, this.state.inputs.address, this.state.inputs.city, this.state.inputs.country, this.state.inputs.name))
+      // addRestaurant(this.state.inputs)
 
     if (Object.keys(this.state.inputs).length)
       this.props.navigate('/' + this.props.type + 's');
@@ -136,7 +137,8 @@ class FormInput extends React.Component {
             this.setState({
               inputs: {
                 ...this.state.inputs,
-                file: e.target.files[0]
+                file: url
+                // file: e.target.files[0]
               }
 
             })
