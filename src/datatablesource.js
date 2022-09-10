cc06ae1a21537e4f9267f52f62769817946f5df1
nass <cabliveer@gmail.com>
2022-09-10 14:25:16 +0200

    gdv

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -127,7 +127,7 @@ export const userColumns = [
         renderCell: (params) => {
           return (
             <div className="cellWithImg">
-              <img className="cellImg" src={params.row.image_url} alt="avatar" />
+              <img className="cellImg" src={params.row.image} alt="avatar" />
               {params.row.name}
             </div>
           );
