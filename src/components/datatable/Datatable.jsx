177b4fbdcac1988cc7482a2c52212bc8c828089e
nass <cabliveer@gmail.com>
2022-09-12 16:24:11 +0200

    vd

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,6 +1,7 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Datatable = ({ rows, columns }) => {
   const [tab, setTab] = useState({})
