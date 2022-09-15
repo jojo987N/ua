b992fbe0ddaf30046a2927371cb6796532fa2974
nass <cabliveer@gmail.com>
2022-09-15 21:57:38 +0200

    save

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,19 @@ where, onSnapshot, serverTimestamp, updateDoc, Timestamp} from 'firebase/firesto
 import { getAuth } from 'firebase/auth';
 
  const firebaseConfig = {
-    apiKey: "AIzaSyDrKqjM-fKGWBqj0-wpOOrIbeVlViEW-3c",
-    authDomain: "good-food-c84d4.firebaseapp.com",
-    projectId: "good-food-c84d4",
-    storageBucket: "good-food-c84d4.appspot.com",
-    messagingSenderId: "716731554402",
-    appId: "1:716731554402:web:bc8a1748f6cdd6885e8f3b",
-    measurementId: "G-VLK10R4D2P"
+
+  apiKey: {/* Your firebase config here */},
+  
+  authDomain: {/* Your firebase config here */},
+
+  projectId: {/* Your firebase config here */},
+
+  storageBucket: {/* Your firebase config here */},
+
+  messagingSenderId: {/* Your firebase config here */},
+
+  appId: {/* Your firebase config here */},
+  
   };
   export const firebaseApp = initializeApp(firebaseConfig);
   export const auth = getAuth(firebaseApp)
